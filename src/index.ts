c1dbac35a61f653e46e0e8f1acef993ea4a940c1
Max <email2anmelden@gmail.com>
2023-05-17 21:12:06 +0200

    add main scripts

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import {
+    Client,
+    IntentsBitField,
+    Message,
+    WebhookClient
+} from 'discord.js';
+
+require('dotenv').config();
+
+const MessageForwards = JSON.parse(process.env.FORWARD_CONFIG || "{}");
+const FractionTag = process.env.FRACTION_TAG || "";
+if (MessageForwards === {}) {
+    console.error("Ungültige/fehlende Konfiguration MESSAGE_FORWARDS");
+    process.exit(1);
+}
+if (FractionTag === "") {
+    console.error("Ungültige/fehlende Konfiguration FRACTION_TAG // Du musst eine Kennung für deine Fraktion angeben");
+    process.exit(1);
+}
+
+const discord = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+discord.login(process.env.DISCORD_TOKEN);
+discord.on('ready', (client: Client) => {
+    if (!discord.user) return;
+    console.log(`Logged in as ${discord.user.tag}!`);
+    discord.user.setPresence({
+        activities: [{name: `Leitet Nachrichten weiter`}],
+        status: 'online'
+    });
+});
+discord.on('messageCreate', async (message: Message) =>{
+    if (message.author.bot) return;
+    console.log(`Forwarding FFW Message from ${message.author.username} to FFW Discords`);
+    for (let webhookURL of MessageForwards[message.channelId]) {
+        const webhookClient = new WebhookClient({url: webhookURL});
+        await webhookClient.send({
+            content: message.content,
+            username: `${message.member?.displayName || message.author.username} (${FractionTag})`,
+            // Avatar des Nutzers, sonst RD-Logo, sonst undefined
+            avatarURL: message.author.avatarURL() || discord.user?.avatarURL() || undefined,
+            files: message.attachments.map(a => a.url),
+        });
+    }
+});
+discord.on('error', console.error);
